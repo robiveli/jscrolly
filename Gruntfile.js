8c472b79b896ad8fe8beebce6fa1970c063ccb41
Your Name <roby@rvdizajn.com>
2017-01-06 09:30:56 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        settings: {
+            srcPath: 'src/',
+            distPath: 'dist/'
+        },
+
+        sass: {
+            app: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= settings.srcPath %>sass',
+                    src: ['**/*.scss'],
+                    dest: '<%= settings.distPath %>css',
+                    ext: '.css'
+                }],
+                options: {
+                    outputStyle: 'compressed',
+                    sourceMap: false,
+                    precision: 5
+                }
+            }
+        },
+
+        uglify: {
+            my_target: {
+                files: {
+                    '<%= settings.distPath %>js/*.js': '<%= settings.srcPath %>js/*.js'
+
+                } 
+            }
+        },
+
+        htmlbuild: {
+            options: {
+                beautify: false,
+                relative: true,
+            },
+            build: {
+                expand: true,
+                cwd: '<%= settings.srcPath %>',
+                src: '**/*.html',
+                dest: '<%= settings.distPath %>'
+            }
+        },
+
+        watch: {
+            javascript: {
+                expand: true,
+                files: ['<%= settings.srcPath %>js/**/*.js', 'Gruntfile.js'],
+                tasks: ['uglify'],
+                options: {
+                    spawn: false
+                }
+            },
+            scss: {
+                expand: true,
+                files: ['<%= settings.srcPath %>sass/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false
+                }
+            },
+            html: {
+                files: ['<%= settings.srcPath %>*.html'],
+                tasks: ['htmlbuild'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass', 'uglify', 'htmlbuild']);
+
+};
